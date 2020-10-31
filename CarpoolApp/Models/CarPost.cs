c6349b46a51c49d1555388d07895307faa91f7f3
Ruoyuan Liu <liuruoyuan1997@gmail.com>
2020-10-31 17:05:04 -0400

    Setup basic and skeleton features

---
+++ CarpoolApp/Models/CarPost.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarpoolApp.Models
+{
+    public class CarPost
+    {
+        public long Id { get; set; }
+        public string Departure { get; set; }
+        public string Destination { get; set; }
+        public List<string> Dropoff { get; set; }
+        public DateTime DimeBegin { get; set; }
+        public DateTime DimeEnd { get; set; }
+        public int Dccupancy { get; set; }
+        public int Luggage { get; set; }
+        public string Note { get; set; }
+
+    }
+}
--- CarpoolApp/Startup.cs
+++ CarpoolApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarpoolApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using CarpoolApp.Models;
 
 namespace CarpoolApp
 {
@@ -24,7 +27,9 @@ namespace CarpoolApp
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+         {
+        //    services.AddDbContext<CarPostContext>(opt =>
+        //       opt.UseInMemoryDatabase("CarPostList"));
             services.AddControllers();
         }
 
